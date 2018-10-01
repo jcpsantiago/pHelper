027ecd802bfc5697d842210b29746471f6f16b08
jcpsantiago <github.jcpsantiago@spamgourmet.com>
2018-10-01 16:24:26 +0200

    more robust full_mean with tests, and custom string

--- R/full_mean.R
+++ R/full_mean.R
@@ -2,6 +2,8 @@
 #' @description Create a sentence comparing two simple means (e.g. from simple_means)
 #' @param means character vector of means.
 #' @param group grouping column (e.g. "treatment")
+#' @param comparison_string what word/expression do you want to use to compare
+#' the means?, Default: "vs"
 #' @return A string with the format "Mean ± SEM unit vs Mean ± SEM"
 #' @examples
 #' \dontrun{
@@ -16,7 +18,7 @@
 #' }
 #' @rdname full_mean
 #' @export
-full_mean <- function(means, group){
+full_mean <- function(means, group, comparison_string = "vs"){
   
   if(!is.character(means))
     stop("Please provide means as character vector!")
@@ -25,6 +27,9 @@ full_mean <- function(means, group){
      (is.null(means) | length(means) != 2))
     stop("means and group must have exactly length 2.")
   
-  paste(means[1], "for", group[1], "vs",
+  if(!is.character(comparison_string))
+    stop("The word/sentence used to compare the means must be a character vector.")
+  
+  paste(means[1], "for", group[1], comparison_string,
         means[2], "for", group[2])
 }
--- tests/testthat/test-full_mean.R
+++ tests/testthat/test-full_mean.R
@@ -15,3 +15,19 @@ test_that("fail when no mean/group or a mean/group with length != 2", {
   
   expect_error(full_mean(group = c("tx1", "tx2")))
 })
+
+test_that("fail if the character string is not a character vector", {
+  expect_error(full_mean(c("mean1", "mean2"),
+                         group = c("tx1", "tx2"),
+                         comparison_string = 999))
+})
+
+test_that("correctly prints out two means with the selected string", {
+  expect_equal(full_mean(c("mean1", "mean2"),
+                         group = c("tx1", "tx2")),
+               "mean1 for tx1 vs mean2 for tx2")
+  expect_equal(full_mean(c("mean1", "mean2"),
+                         group = c("tx1", "tx2"),
+                         comparison_string = "compared with"),
+               "mean1 for tx1 compared with mean2 for tx2")
+})
